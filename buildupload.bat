0e0d3f69a3a2617a934e45b5c128b47af78abf14
codemicro <thomasdpain@gmail.com>
2019-09-28 10:49:22 +0100

    Updated buildupload.bat

--- buildupload.bat
+++ buildupload.bat
@@ -1,2 +1,29 @@
+@echo off
+setlocal
+:PROMPT
+echo This will compile new wheels and source code archives, as well as compile new documentation.
+SET /P AREYOUSURE=Are you sure (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+echo.
+echo Deleting old wheels
+cd dist
+del *.*
+cd ..
+echo.
+echo Compiling new wheels
 python setup.py sdist bdist_wheel
+
+echo.
+SET /P AREYOUSURE=Would you like to upload the new compiled source code? (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO ENDONE
+echo Uploading to PyPI
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+:ENDONE
+
+echo.
+echo Compiling docs
+mkdocs build
+
+:END
+endlocal
