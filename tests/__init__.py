c22a001353082186311e5d7899195700cd1a995f
codemicro <thomasdpain@gmail.com>
2019-09-30 18:52:02 +0100

    Prevented Flask from outputting stuff during tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,10 @@ import threading
 
 from tests.webserver import app
 
+import logging
+
+log = logging.getLogger('werkzeug')
+log.disabled = True
 
 def flaskserver():
     app.run(host='127.0.0.1', port=5000, debug=False)
